3696d79ed415945c2f65d51c6f0765701fedcc52
Morgan <gooldn000@gmail.com>
2023-02-17 01:21:57 +0300

    Added new file MainForm.cs

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnGo = new System.Windows.Forms.Button();
             this.txtUrl = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -45,6 +45,10 @@
             this.btnPath = new System.Windows.Forms.Button();
             this.ofd = new System.Windows.Forms.FolderBrowserDialog();
             this.webView = new EO.WebBrowser.WebView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblLike = new System.Windows.Forms.Label();
+            this.labelDislike = new System.Windows.Forms.Label();
+            this.lblDislike = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnGo
@@ -59,35 +63,36 @@
             // 
             // txtUrl
             // 
-            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.BackColor = System.Drawing.SystemColors.Control;
+            this.txtUrl.Location = new System.Drawing.Point(15, 12);
             this.txtUrl.Name = "txtUrl";
-            this.txtUrl.PlaceholderText = "Enter Video Link ..";
-            this.txtUrl.Size = new System.Drawing.Size(500, 23);
+            this.txtUrl.PlaceholderText = "Enter Video Link or Video Id ..";
+            this.txtUrl.Size = new System.Drawing.Size(497, 23);
             this.txtUrl.TabIndex = 2;
             // 
-            // button1
+            // btnCancel
             // 
-            this.button1.Location = new System.Drawing.Point(89, 450);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Cancel";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnCancel.Location = new System.Drawing.Point(89, 450);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Location = new System.Drawing.Point(12, 82);
+            this.panel1.Location = new System.Drawing.Point(15, 82);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(500, 173);
+            this.panel1.Size = new System.Drawing.Size(497, 173);
             this.panel1.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(8, 268);
+            this.label1.Location = new System.Drawing.Point(15, 268);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(38, 15);
             this.label1.TabIndex = 5;
@@ -97,7 +102,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(8, 304);
+            this.label2.Location = new System.Drawing.Point(15, 292);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 6;
@@ -107,7 +112,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(8, 379);
+            this.label3.Location = new System.Drawing.Point(15, 389);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 15);
             this.label3.TabIndex = 7;
@@ -117,7 +122,7 @@
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblTitle.Location = new System.Drawing.Point(43, 270);
+            this.lblTitle.Location = new System.Drawing.Point(49, 270);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(35, 13);
             this.lblTitle.TabIndex = 8;
@@ -127,7 +132,7 @@
             // 
             this.lblAuthor.AutoSize = true;
             this.lblAuthor.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblAuthor.Location = new System.Drawing.Point(57, 306);
+            this.lblAuthor.Location = new System.Drawing.Point(63, 294);
             this.lblAuthor.Name = "lblAuthor";
             this.lblAuthor.Size = new System.Drawing.Size(35, 13);
             this.lblAuthor.TabIndex = 9;
@@ -137,7 +142,7 @@
             // 
             this.lblDate.AutoSize = true;
             this.lblDate.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDate.Location = new System.Drawing.Point(90, 381);
+            this.lblDate.Location = new System.Drawing.Point(94, 391);
             this.lblDate.Name = "lblDate";
             this.lblDate.Size = new System.Drawing.Size(35, 13);
             this.lblDate.TabIndex = 10;
@@ -145,6 +150,7 @@
             // 
             // progressBar1
             // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
             this.progressBar1.Location = new System.Drawing.Point(12, 417);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(500, 23);
@@ -154,7 +160,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(8, 342);
+            this.label4.Location = new System.Drawing.Point(15, 316);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 15);
             this.label4.TabIndex = 12;
@@ -164,7 +170,7 @@
             // 
             this.lblViews.AutoSize = true;
             this.lblViews.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblViews.Location = new System.Drawing.Point(53, 344);
+            this.lblViews.Location = new System.Drawing.Point(57, 318);
             this.lblViews.Name = "lblViews";
             this.lblViews.Size = new System.Drawing.Size(35, 13);
             this.lblViews.TabIndex = 13;
@@ -172,10 +178,11 @@
             // 
             // txtPath
             // 
-            this.txtPath.Location = new System.Drawing.Point(12, 48);
+            this.txtPath.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPath.Location = new System.Drawing.Point(15, 48);
             this.txtPath.Name = "txtPath";
             this.txtPath.PlaceholderText = "Enter Path ..";
-            this.txtPath.Size = new System.Drawing.Size(470, 23);
+            this.txtPath.Size = new System.Drawing.Size(467, 23);
             this.txtPath.TabIndex = 15;
             // 
             // btnPath
@@ -194,12 +201,56 @@
             this.webView.ObjectForScripting = null;
             this.webView.Title = null;
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(15, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Like:";
+            // 
+            // lblLike
+            // 
+            this.lblLike.AutoSize = true;
+            this.lblLike.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblLike.Location = new System.Drawing.Point(46, 341);
+            this.lblLike.Name = "lblLike";
+            this.lblLike.Size = new System.Drawing.Size(35, 13);
+            this.lblLike.TabIndex = 18;
+            this.lblLike.Text = "None";
+            // 
+            // labelDislike
+            // 
+            this.labelDislike.AutoSize = true;
+            this.labelDislike.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelDislike.Location = new System.Drawing.Point(15, 364);
+            this.labelDislike.Name = "labelDislike";
+            this.labelDislike.Size = new System.Drawing.Size(47, 15);
+            this.labelDislike.TabIndex = 19;
+            this.labelDislike.Text = "Dislike:";
+            // 
+            // lblDislike
+            // 
+            this.lblDislike.AutoSize = true;
+            this.lblDislike.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDislike.Location = new System.Drawing.Point(63, 366);
+            this.lblDislike.Name = "lblDislike";
+            this.lblDislike.Size = new System.Drawing.Size(35, 13);
+            this.lblDislike.TabIndex = 20;
+            this.lblDislike.Text = "None";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(524, 481);
+            this.Controls.Add(this.lblDislike);
+            this.Controls.Add(this.labelDislike);
+            this.Controls.Add(this.lblLike);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnPath);
             this.Controls.Add(this.txtPath);
             this.Controls.Add(this.lblViews);
@@ -212,7 +263,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.txtUrl);
             this.Controls.Add(this.btnGo);
             this.Name = "MainForm";
@@ -243,5 +294,10 @@
         private Button btnPath;
         private FolderBrowserDialog ofd;
         private EO.WebBrowser.WebView webView;
+        private Label label5;
+        private Label lblLike;
+        private Label labelDislike;
+        private Label lblDislike;
+        private Button btnCancel;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -6,31 +6,45 @@ using System.Runtime.ConstrainedExecution;
 using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace YouDLl
 {
     public partial class MainForm : Form
     {
-        ChromiumWebBrowser browser;
+        sbyte progessBarRed = 2;
         public MainForm()
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             webView.Create(panel1.Handle);
+            ModifyProgressBarColor.SetState(progressBar1, progessBarRed);
         }
-
-        private async void btnGo_Click(object sender, EventArgs e)
+        private String CheckUrlorId(string text)
         {
+            try
+            {
+                string baseUrl = text.Split("/")[2];
+            } catch
+            {
+                return "https://www.youtube.com/watch?v=" + text;
+            }
+            return text;
+        }
+        private async void VideoDownload()
+        {
+            var url = CheckUrlorId(txtUrl.Text);
             var youtube = new YoutubeClient();
-            var video = await youtube.Videos.GetAsync(txtUrl.Text);
+            var video = await youtube.Videos.GetAsync(url);
             var title = video.Title;
             var author = video.Author.ChannelTitle;
             var views = video.Engagement.ViewCount.ToString();
+            var likes = video.Engagement.LikeCount.ToString();
+            var dislikes = video.Engagement.DislikeCount.ToString();
             var date = video.UploadDate.Date.ToString();
-            var videoId = txtUrl.Text.Split("?v=")[1];
+            var videoId = url.Split("?v=")[1];
 
             webView.Url = "https://www.youtube.com/embed/" + videoId;
 
@@ -39,6 +53,8 @@ namespace YouDLl
             lblTitle.Text = title;
             lblAuthor.Text = author;
             lblViews.Text = views;
+            lblLike.Text = likes;
+            lblDislike.Text = dislikes;
             lblDate.Text = date;
 
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(videoId);
@@ -50,15 +66,27 @@ namespace YouDLl
             });
 
             await youtube.Videos.Streams.DownloadAsync(streamInfo, ofd.SelectedPath + "\\" + fileName, progress);
-            this.Text = "Youdli";
-            progressBar1.Value = 0;
             MessageBox.Show("the Video download Completed");
         }
-
+        private async void btnGo_Click(object sender, EventArgs e)
+        {
+            VideoDownload();
+        }
         private void btnPath_Click(object sender, EventArgs e)
         {
             ofd.ShowDialog();
             txtPath.Text = ofd.SelectedPath.ToString();
         }
+
     }
+    public static class ModifyProgressBarColor
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
+        public static void SetState(this ProgressBar pBar, int state)
+        {
+            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
+        }
+    }
+
 }
